b430c772d50c669f11448d91e1d1c2a04f2f93ff
claudiavargas <claudializethvargasramirez@gmail.com>
2020-12-02 21:11:46 -0500

    modificacion anio

---
+++ main/java/co/edu/cotecnova/holamundo/holamundoapplication.java
@@ -0,0 +1,7 @@
+package main.java.co.edu.cotecnova.holamundo;
+
+public class holamundoapplication {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo 2020");
+    }
+}
